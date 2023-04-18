61f65bea65743305673a42b58d635ad180aee9ef
MuratHaktanir <79758626+MuratHaktanir@users.noreply.github.com>
2023-04-18 17:59:59 +0300

    Initial Commit

---
+++ quizApp/quizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizAppApp.swift
+//  quizApp
+//
+//  Created by Murat Haktanır on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct quizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
